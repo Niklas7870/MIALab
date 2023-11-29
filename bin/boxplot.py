f6bac766bd300be0a53c2a3911e48c9c8772bbea
Brunohilde <s.willingsdorfer@gmail.com>
2023-11-29 11:30:57 +0100

    Boxplot updated - checks if Boxplot was done per folder and does it if not

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -19,51 +19,55 @@ def main():
             if os.path.isdir(bd): result.append(bd)
         return result
 
-    directory = max(all_subdirs_of(), key=os.path.getmtime)
+    all_subdirs = all_subdirs_of()
+    for subdirs in all_subdirs:
+        if os.path.exists(os.path.join(subdirs, "Dice_allL.png")):
+            continue
 
-    #foldername = "2023-11-07-14-12-44" # must be adjusted
-    #directory = os.path.join('mia-result', foldername)
-    filename = "results.csv"
-    filepath = os.path.join(directory, filename)
-    data = pd.read_csv(filepath, sep=';', header=0, index_col=["SUBJECT"])
+        directory = subdirs
+        #foldername = "2023-11-07-14-12-44" # must be adjusted
+        #directory = os.path.join('mia-result', foldername)
+        filename = "results.csv"
+        filepath = os.path.join(directory, filename)
+        data = pd.read_csv(filepath, sep=';', header=0, index_col=["SUBJECT"])
 
-    # Plot Parameter
-    diceName = 'DICE'
-    diceYlim = [0, 1]
-    HDRFDSTName = 'HDRFDST'
-    HDRFDSTYlim = [0, 20] # upper limit probably must be adjusted
-    subjectName = 'Subject'
-    labelName = 'Label'
+        # Plot Parameter
+        diceName = 'DICE'
+        diceYlim = [0, 1]
+        HDRFDSTName = 'HDRFDST'
+        HDRFDSTYlim = [0, 20] # upper limit probably must be adjusted
+        subjectName = 'Subject'
+        labelName = 'Label'
 
-    # dice and housdorffdistance per subject (over all labels)
-    Dice_allS = data.boxplot(by='SUBJECT', column=['DICE'], rot=45, grid=False).get_figure()
-    plt.xlabel(subjectName)
-    plt.ylabel(diceName)
-    plt.ylim(diceYlim)
-    HDRFDST_allS = data.boxplot(by='SUBJECT', column=['HDRFDST'], rot=45, grid=False).get_figure()
-    plt.xlabel(subjectName)
-    plt.ylabel(HDRFDSTName)
-    plt.ylim(HDRFDSTYlim)
+        # dice and housdorffdistance per subject (over all labels)
+        Dice_allS = data.boxplot(by='SUBJECT', column=['DICE'], rot=45, grid=False).get_figure()
+        plt.xlabel(subjectName)
+        plt.ylabel(diceName)
+        plt.ylim(diceYlim)
+        HDRFDST_allS = data.boxplot(by='SUBJECT', column=['HDRFDST'], rot=45, grid=False).get_figure()
+        plt.xlabel(subjectName)
+        plt.ylabel(HDRFDSTName)
+        plt.ylim(HDRFDSTYlim)
 
-    # dice and housdorffdistance per label (over all subjects)
-    Dice_allL = data.boxplot(by='LABEL', column=['DICE'], grid=False).get_figure()
-    plt.xlabel(labelName)
-    plt.ylabel(diceName)
-    plt.ylim(diceYlim)
-    HDRFDST_allL = data.boxplot(by='LABEL', column=['HDRFDST'], grid=False).get_figure()
-    plt.xlabel(labelName)
-    plt.ylabel(HDRFDSTName)
-    plt.ylim(HDRFDSTYlim)
+        # dice and housdorffdistance per label (over all subjects)
+        Dice_allL = data.boxplot(by='LABEL', column=['DICE'], grid=False).get_figure()
+        plt.xlabel(labelName)
+        plt.ylabel(diceName)
+        plt.ylim(diceYlim)
+        HDRFDST_allL = data.boxplot(by='LABEL', column=['HDRFDST'], grid=False).get_figure()
+        plt.xlabel(labelName)
+        plt.ylabel(HDRFDSTName)
+        plt.ylim(HDRFDSTYlim)
 
-    # save figures
-    Dice_allS.tight_layout()
-    Dice_allS.savefig(os.path.join(directory, 'Dice_allS.png'), dpi=600)
-    HDRFDST_allS.tight_layout()
-    HDRFDST_allS.savefig(os.path.join(directory, 'HDRFDST_allS.png'), dpi=600)
-    Dice_allL.tight_layout()
-    Dice_allL.savefig(os.path.join(directory, 'Dice_allL.png'), dpi=600)
-    HDRFDST_allL.tight_layout()
-    HDRFDST_allL.savefig(os.path.join(directory, 'HDRFDST_allL.png'), dpi=600)
+        # save figures
+        Dice_allS.tight_layout()
+        Dice_allS.savefig(os.path.join(directory, 'Dice_allS.png'), dpi=600)
+        HDRFDST_allS.tight_layout()
+        HDRFDST_allS.savefig(os.path.join(directory, 'HDRFDST_allS.png'), dpi=600)
+        Dice_allL.tight_layout()
+        Dice_allL.savefig(os.path.join(directory, 'Dice_allL.png'), dpi=600)
+        HDRFDST_allL.tight_layout()
+        HDRFDST_allL.savefig(os.path.join(directory, 'HDRFDST_allL.png'), dpi=600)
 
     #plt.show()
 
