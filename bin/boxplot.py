092983b83d18645c8b4e57157ea573afb61f09e7
Brunohilde <s.willingsdorfer@gmail.com>
2023-12-13 08:43:13 +0100

    Gaussian & SP added

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -46,43 +46,60 @@ def main():
         plt.xlabel(subjectName)
         plt.ylabel(diceName)
         plt.ylim(diceYlim)
+        plt.close()
         HDRFDST_allS = data.boxplot(by='SUBJECT', column=[HDRFDSTName], rot=45, grid=False).get_figure()
         plt.xlabel(subjectName)
         plt.ylabel(HDRFDSTName)
         plt.ylim(HDRFDSTYlim)
-        Accuracy_allS = data.boxplot(by='SUBJECT', column=[accuracyName], rot=45, grid=False).get_figure()
-        plt.xlabel(subjectName)
-        plt.ylabel(accuracyName)
-        plt.ylim(accuracyYlim)
+        plt.close()
+        try:
+            Accuracy_allS = data.boxplot(by='SUBJECT', column=[accuracyName], rot=45, grid=False).get_figure()
+            plt.xlabel(subjectName)
+            plt.ylabel(accuracyName)
+            plt.ylim(accuracyYlim)
+            plt.close()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
         # DICE, HDRFDST, ACCURACY per label (over all subjects)
         Dice_allL = data.boxplot(by='LABEL', column=[diceName], grid=False).get_figure()
         plt.xlabel(labelName)
         plt.ylabel(diceName)
         plt.ylim(diceYlim)
+        plt.close()
         HDRFDST_allL = data.boxplot(by='LABEL', column=[HDRFDSTName], grid=False).get_figure()
         plt.xlabel(labelName)
         plt.ylabel(HDRFDSTName)
         plt.ylim(HDRFDSTYlim)
-        Accuracy_allL = data.boxplot(by='LABEL', column=[accuracyName], grid=False).get_figure()
-        plt.xlabel(labelName)
-        plt.ylabel(accuracyName)
-        plt.ylim(accuracyYlim)
+        plt.close()
+        try:
+            Accuracy_allL = data.boxplot(by='LABEL', column=[accuracyName], grid=False).get_figure()
+            plt.xlabel(labelName)
+            plt.ylabel(accuracyName)
+            plt.ylim(accuracyYlim)
+            plt.close()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
         # save figures
         Dice_allS.tight_layout()
         Dice_allS.savefig(os.path.join(directory, 'Dice_allS.png'), dpi=600)
         HDRFDST_allS.tight_layout()
         HDRFDST_allS.savefig(os.path.join(directory, 'HDRFDST_allS.png'), dpi=600)
-        Accuracy_allS.tight_layout()
-        Accuracy_allS.savefig(os.path.join(directory, 'Accuracy_allS.png'), dpi=600)
+        try:
+            Accuracy_allS.tight_layout()
+            Accuracy_allS.savefig(os.path.join(directory, 'Accuracy_allS.png'), dpi=600)
+        except Exception as e:
+            print(f"An error occurred: {e}")
         Dice_allL.tight_layout()
         Dice_allL.savefig(os.path.join(directory, 'Dice_allL.png'), dpi=600)
         HDRFDST_allL.tight_layout()
         HDRFDST_allL.savefig(os.path.join(directory, 'HDRFDST_allL.png'), dpi=600)
-        Accuracy_allL.tight_layout()
-        Accuracy_allL.savefig(os.path.join(directory, 'Accuracy_allL.png'), dpi=600)
-
+        try:
+            Accuracy_allL.tight_layout()
+            Accuracy_allL.savefig(os.path.join(directory, 'Accuracy_allL.png'), dpi=600)
+        except Exception as e:
+            print(f"An error occurred: {e}")
     #plt.show()
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
